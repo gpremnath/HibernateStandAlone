2d4d1844443a5cf30081f64dfe3338d388ecfa4c
gpremnath <prem_nathg@yahoo.com>
2016-04-29 12:25:34 +0530

    first commit

---
+++ sql-scripts/create.sql
@@ -0,0 +1,3 @@
+create table student_address(address_id serial primary key, street text, city text, country text);
+create table student(student_id serial primary key, address_id int , first_name varchar(30), last_name varchar(30), section varchar(15));
+alter table student add constraint fk_student_address foreign key(address_id) references student_address;
