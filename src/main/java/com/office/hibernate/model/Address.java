722b94505f88fcf3ab2f5618f9b498cc2c48fd87
gpremnath <prem_nathg@yahoo.com>
2016-04-29 19:35:37 +0530

    phase 1

--- src/main/java/com/office/hibernate/model/Address.java
+++ src/main/java/com/office/hibernate/model/Address.java
@@ -1,3 +1,88 @@
 package com.office.hibernate.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Table(name="student_address")
+public class Address {
+
+	@Id
+	@GeneratedValue
+	@Column(name="address_id")
+	Integer addressId;
+	
+	@Column(name="street")
+	String street;
+	
+	@Column(name="city")
+	String city;
+
+	@Column(name="country")
+	String country;
+
+	public Address(){}	
+
+	public Address(String street,String city,String country){
+		this.street=street;
+		this.city=city;
+		this.country=country;
+	}	
+
+
+	public void setAddressId(Integer addressId){
+		this.addressId=addressId;
+	}	
+
+
+	public Integer getAddressId(){
+		return addressId;
+	}
+
+	
+	public void setStreet(String street){
+		this.street=street;
+	}	
+
+
+	public String getStreet(){
+		return street;
+	}	
+
+	
+	public void setCity(String city){
+		this.city=city;
+	}	
+
+
+	public String getCity(){
+		return city;
+	}	
+
+	
+	public void setCountry(String country){
+		this.country=country;
+	}	
+
+
+	public String getCountry(){
+		return country;
+	}	
+
+	
+	@Override
+    	public String toString() {
+    	
+		return "Address [id=" + addressId.toString()+ ", street=" + street + ", city=" + city
+                	+ ", country=" + country + "]";
+    
+	}	
+	
+
+}
+
---
+++ src/test/java/com/office/test/AppTest.java
@@ -0,0 +1,36 @@
+package com.office.test;
+
+import org.hibernate.Session;
+import org.junit.Test;
+import com.office.hibernate.model.Address;
+import com.office.hibernate.model.Student;
+import com.office.hibernate.util.HibernateUtil;
+import java.util.List;
+
+public class  AppTest {
+
+@Test
+public void InsertTest() {
+		Student student= new Student("Sam","Disilva","Maths");
+		Address address= new Address("10 Silver street","NYC","USA");
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.persist(address);
+		student.setAddress(address);
+		session.persist(student);
+		session.getTransaction().commit();
+
+		List <Student> students = (List <Student> )session.createQuery(" from Student").list();
+		for ( Student s: students){
+			System.out.println("Details "+s);
+		}
+
+		session.close();
+
+	}
+
+
+
+
+}
